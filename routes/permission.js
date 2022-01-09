dd91ee010019c7696f500e30d033462f0b1231e7
antonoeschmidt <s163053@student.dtu.dk>
2022-01-09 14:52:46 +0100

    Delete permission

--- routes/permission.js
+++ routes/permission.js
@@ -68,4 +68,13 @@ router.put("/:id", async (req, res) => {
     }
 });
 
+router.delete("/:id", async (req, res) => {
+    try {
+        await Permission.findByIdAndDelete(req.params.id);
+        res.status(200).json({message: "Permission deleted"});
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
